f00bc97d98adcbb93245caabb634a9d38abd8c4d
chaslinux <chaslinux@gmail.com>
2025-09-26 14:48:11 -0400

    Install dependencies based on website.

--- install-pyenv.sh
+++ install-pyenv.sh
@@ -2,12 +2,11 @@
 
 # install-pyenv.sh
 
+sudo apt update
 sudo apt install make build-essential libssl-dev zlib1g-dev -y
-sudo apt install libbz2-dev libreadline-dev libsqlite3-dev wget -y
-sudo apt install curl llvm libncurses5-dev libncursesw5-dev -y
-sudo apt install xz-utils tk-dev libffi-dev liblzma-dev -y
-sudo apt install python-openssl git -y
-
+sudo apt install libbz2-dev libreadline-dev libsqlite3-dev curl git -y
+sudo apt install libncursesw5-dev xz-utils tk-dev libxml2-dev -y 
+sudo apt install libxmlsec1-dev libffi-dev liblzma-dev -y
 curl https://pyenv.run | bash
 
 export PYENV_ROOT="$HOME/.pyenv" [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
