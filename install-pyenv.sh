ebe22dfb1ea1a79977f86a58f91cdbf08e65d082
chaslinux <chaslinux@gmail.com>
2025-09-26 15:26:43 -0400

    More potential fixes.

--- install-pyenv.sh
+++ install-pyenv.sh
@@ -13,3 +13,5 @@ echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init - bash)"' >> ~/.bashrc
 
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+source ~/.bashrc
